ec8458c142ef3cd7fbdfa8a4834097aa948e01fe
Wesley Coelho <wes.coelho@yahoo.com.br>
2021-08-24 21:47:03 -0300

    First Update

---
+++ script.js
@@ -0,0 +1,24 @@
+/* Menu Mobile */
+
+function menuToggle() {
+    let menuMobile = document.getElementById('menu-mobile');
+    if (menuMobile.style.width == '150px') {
+        menuMobile.style.width = '0px';
+    } else {
+        menuMobile.style.width = '150px';
+    }
+}
+
+/* header */
+
+window.onscroll = function () { rolagem() };
+
+function rolagem() {
+    if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+        document.querySelector("#header").style.background = "linear-gradient(to left, #b4ddb4 0%,#83c783 7%,#52b152 20%,#008a00 45%,#005700 73%,#002400 100%)";
+        document.querySelector("#header").style.borderBottom = "2px solid darkgreen";
+    } else {
+        document.querySelector("#header").style.background = "transparent";
+        document.querySelector("#header").style.borderBottom = "transparent";
+    }
+}
